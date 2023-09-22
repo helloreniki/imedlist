8f01fad66b71e0ed34015cfd287b06dd26e86ac3
Reniki <hello@reniki.com>
2023-09-22 20:59:45 +0200

    dashboard.vue start

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,8 +13,8 @@ import Welcome from '@/Components/Welcome.vue';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-8 py-8 min-h-screen">
+                  <div>Test</div>
                 </div>
             </div>
         </div>
