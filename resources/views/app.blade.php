2add1a1effa80766c4b3bcc8e79ee9462ed8addc
Reniki <hello@reniki.com>
2023-12-19 09:25:13 +0100

    seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="title" content="Medlist: Free printable and online medication list">
+        <meta name="description" content="Free printable online medication list. Create a list of medications for you or someone else, export it to PDF and print or just access it online.">
+        <meta name="keywords" content="pdf medication list, drug list, wallet medicine list, wallet drug list, pdf drug list">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="language" content="English">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
